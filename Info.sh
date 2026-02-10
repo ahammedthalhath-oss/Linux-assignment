988a54b28ddf6edf79770de76922fc22dc973407
ahammedthalhath-oss <ahammedthalhath@gmail.com>
2026-02-10 23:35:28 +0530

    Add student information script Info.sh

---
+++ Info.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "==============================="
+echo " Student Information Script"
+echo "==============================="
+
+echo "Name        : Ahamed Thalhath"
+echo "Roll Number : YOUR_ROLL_NUMBER"
+echo "Class       : YOUR_CLASS"
+
+echo ""
+echo "Current Date and Time:"
+date
+
+echo "==============================="
